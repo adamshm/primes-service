d996ccf35e17b71260b8bf0d192f67f5ff490273
adamshm <adamshm@iu.edu>
2024-04-26 18:04:10 -0400

    pac 13

--- src/main/java/edu/iu/adamshm/primesservice/rabbitmq/MQSender.java
+++ src/main/java/edu/iu/adamshm/primesservice/rabbitmq/MQSender.java
@@ -17,7 +17,7 @@ public class MQSender {
     }
     public void sendMessage(String username, int n, boolean isPrime) {
         String message =
-                MessageFormat.format("customer:{0}, n: {1}, isPrime: {2}", username, n, isPrime);
+                MessageFormat.format("customer:{0}, n: {1}, isPrime: {2}", username, String.valueOf(n), isPrime);
         message = "{" + message + "}";
         rabbitTemplate.convertAndSend("primes", message);
     }
